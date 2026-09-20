e8dd3819e2760fee31021dd91e4a9f52e08712a1
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-09-20 22:43:45 +0300

    Add files via upload

---
+++ DAL/Exceptions/DataAcessException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Exceptions
+{
+    public class DataAcessException:Exception
+    {
+
+
+        public string CustomMessage { get; set; }
+
+        public DataAcessException(
+       string message,
+       string customMessage,
+       Exception innerException): base(message, innerException)
+        {
+            CustomMessage = customMessage;
+        }
+    }
+}
---
+++ DAL/Exceptions/SerlizationnExecption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Exceptions
+{
+    public class SerlizationnExecption:Exception
+    {
+
+
+        public string CustomMessage { get; set; }
+
+
+
+        public SerlizationnExecption(string customMessage, string message,
+            Exception innerException) : base(message, innerException)
+        {
+            CustomMessage = customMessage;
+        }
+
+    }
+}
