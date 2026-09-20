df59d19f14ac7859b982d232951987cb5da65f51
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-09-20 22:42:10 +0300

    Add files via upload

---
+++ DAL/Contracts/IConverter.cs
@@ -0,0 +1,25 @@
+﻿using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Contracts
+{
+    public interface IConverter<T> where T : BaseEntity, new()
+    {
+
+
+        public string ConvertData(List<T> data);
+
+        public List<T> ConvertBack(string fileData);
+
+
+
+
+
+
+
+    }
+}
---
+++ DAL/Contracts/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Contracts
+{
+    public interface IRepository<T>
+    {
+
+
+
+        public Task<List<T>> GetAll();
+
+        public Task<T> GetById(int id);
+
+        public Task Add(T model);
+
+        public Task Update(T model);
+
+        public Task Delete(int id);
+
+
+
+    }
+}
